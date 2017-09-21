56da5655d364d6c789d0c699f47edc09e785eb3f
jasillo <jorge.asillo@ucsp.edu.pe>
2017-09-21 07:58:12 -0500

    estructura del juego

---
+++ juego_1/clientGame.cpp
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+ 
+#include <netdb.h>
+#include <unistd.h>
+#include <thread> 
+ 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <ncurses.h>
+
+#include "Packet.h"
+ 
+#define SERVERPORT 8080 
+#define BUFFSIZE 1024
+#define USERNAMESIZE 2
+#define COMMANDSIZE 1
+
+using namespace std;
+
+int sockfd;
+bool isconnected;
+string user;
+
+// se encarga de recivir los mensajes
+void receiver(){
+    int r , tam;
+    char buffer[BUFFSIZE], headBuffer[4];
+    bzero(buffer,BUFFSIZE);
+    bzero(headBuffer,4);
+    struct PACKET recvPacket;
+    int headerSize = USERNAMESIZE + COMMANDSIZE;
+
+    while (isconnected){
+        //recivimos tamaño del mensaje
+        r = recv (sockfd, headBuffer, headerSize, 0);
+        if (r < 0){
+            cout<<"se perdio la coneccion"<<endl;
+            isconnected = false;
+            close(sockfd);
+            break;
+        }
+        
+        recvPacket.analizeHeader(headBuffer);
+        //opciones del header
+        if (recvPacket.opt == "m"){
+            //leer lo que queda del paquete y dibujar lo necesario
+        }
+        else if (recvPacket.opt == "x"){
+
+        }
+        else if (recvPacket.opt == "s"){
+
+        }
+        else if (recvPacket.opt == "c"){ //chat
+
+        }
+        else {
+            // opcion no reconocida
+        }
+        
+
+        bzero(buffer,BUFFSIZE);
+        bzero(headBuffer,headerSize);
+    }
+};
+
+//enviar mensajes a todos los usuarios
+void sendtoall(string msg) {
+        
+    if(!isconnected) {
+        cout<<"no estas conectado"<<endl;
+        return;
+    } 
+    
+    struct PACKET sendPacket;
+    sendPacket.opt = "c"; //chat
+    sendPacket.user = user;
+    sendPacket.message = msg;
+    string sendMsg = sendPacket.generate(); 
+    int s = send(sockfd, sendMsg.c_str(), sendMsg.size(),0);
+    
+    if (s < 0){        
+        cout<<"error al enviar mensaje"<<endl;
+    }
+}
+
+
+// se conecta al servidor
+void login(string u) {    
+    
+    if(isconnected) {
+        cout<<"ya estas conectado al servidor"<<endl;        
+        return;
+    }
+
+    int servidor;         
+    string ip = "127.0.0.1";
+    struct  sockaddr_in direc; 
+
+    if((servidor = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
+        cout<<"error al crear el socket"<<endl;
+        exit(0);
+    }  
+
+    direc.sin_family = AF_INET;
+    direc.sin_port = htons(SERVERPORT);
+    inet_pton(AF_INET, ip.c_str(), &direc.sin_addr);
+
+    if (connect(servidor,(struct sockaddr *)&direc, sizeof(direc)) == -1 ){
+        cout<<"conexion al servidor fallo"<<endl;
+        return;
+    } 
+    sockfd = servidor;
+    user = u.substr(0, USERNAMESIZE);
+    isconnected = true;
+    thread (receiver).detach();
+
+   //enviar posicion inicial
+}
+
+//se desconecta del servidor
+void logout() {
+    
+    int sent;
+    struct PACKET exitPacket;
+    
+    if(!isconnected) {
+        cout<<"ya estas desconectado"<<endl;
+        return;
+    }
+    isconnected = false;
+
+    exitPacket.opt = "e"; //opcion salir
+    exitPacket.user = user;
+    exitPacket.message = "";
+    string exitMessage = exitPacket.generate();
+    
+    sent = send(sockfd, exitMessage.c_str(), exitMessage.size(), 0);
+
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
+}
+
+int main(int argc, char **argv) {   
+
+    char buffer[BUFFSIZE];
+    int sockfd, aliaslen; 
+    isconnected = false;
+    string u;
+    
+    cout<<"introducir usuario"<<endl;
+    cin.getline(buffer, BUFFSIZE);
+    s = buffer;
+    login(s);
+
+    cout<<"presionar cualquier tecla para comenzar el juego"<<endl;
+
+    initscr();       
+    noecho();
+    int ch; 
+    while(isconnected) {        
+        if (kbhit()) {
+            /* 
+            ch = = getch();
+            ver que tecla ha sido presionada
+            y enviar el send dependiendo de ello
+
+            */
+        }
+    }
+    return 0;
+};
+ 
+
+ 
+
+ 
---
+++ juego_1/serverGame.cpp
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+ 
+#include <netdb.h>
+#include <unistd.h>
+#include <thread>
+ 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <mutex> 
+
+#include "Packet.h"
+
+#define PORT 8080 
+#define BUFFSIZE 1024
+#define USERNAMESIZE 2
+
+using namespace std;
+
+vector<int> clientsFD;
+ 
+int sockfd;
+bool isconnected;
+mutex mtxScreen;
+
+// interfaz del servidor que lee commandos de la pantalla (thread)
+void handler(){
+    char buffer[BUFFSIZE]; 
+    
+    while(cin.getline(buffer, BUFFSIZE)) {
+        //analizar linea de comando dentro de packet
+        string line = buffer;
+        string option = getWord(&line);        
+        // ejecutar la opcion elegida
+        if (option == "quit"){            
+            isconnected = false;
+            shutdown(sockfd, SHUT_RDWR);
+            close(sockfd);;
+            break;
+        }       
+        else{
+            mtxScreen.lock();
+            cout<<"orden no reconocida"<<endl;
+            mtxScreen.unlock();
+        }
+
+        bzero(buffer,BUFFSIZE);
+    }
+    mtxScreen.lock();
+    cout<<"servidor terminado"<<endl;
+    mtxScreen.unlock();
+};
+
+// manejador que recive los mensajes de los clientes y los renvia a los demas (thread)
+void clientHandler(int fd){
+    int r, tam;
+    string msg;
+    char headBuffer[4], buffer[BUFFSIZE];
+    struct PACKET recvPacket;
+
+    while (isconnected){
+        r = recv (fd, headBuffer, 4, 0); // recive el tamaño de la cabezera
+        if (r < 0){
+            cout<<"se perdio la coneccion"<<endl;            
+            close(fd);
+            break;
+        }
+        
+        recvPacket.analizeHeader(headBuffer);
+        //opciones del header
+        if (recvPacket.opt == "m"){
+            //leer lo que queda del paquete y reenviar
+        }
+        else if (recvPacket.opt == "x"){
+
+        }
+        else if (recvPacket.opt == "s"){
+
+        }
+        else if (recvPacket.opt == "c"){ //chat
+
+        }
+        else {
+            // opcion no reconocida
+        } 
+
+        bzero(buffer,BUFFSIZE);
+        bzero(headBuffer,4);
+    }
+
+}
+
+int main(int argc, char **argv) {
+    isconnected = true;
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        cout<<"error al crear el socket"<<endl;
+        return -1;
+    }
+    //struct sockaddr_in direc;
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    memset(&(serv_addr.sin_zero), 0, 8);
+ 
+    if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) == -1) {        
+        cout<<"error al asignar puerto"<<endl;
+        return -1;
+    }
+ 
+    if(listen(sockfd, 10) == -1) {        
+        cout << "error en liste"<<endl;
+        return -1;
+    }
+ 
+    /* initiate interrupt handler for IO controlling */
+    thread (handler).detach();
+ 
+    /* keep accepting connections */
+    socklen_t sin_size = sizeof(serv_addr);;
+    int newfd;
+    while(isconnected) {
+        //accept(servidor, (struct sockaddr*)&direc, &direcSize
+        if((newfd = accept(sockfd, (struct sockaddr *)&serv_addr, &sin_size)) == -1) {
+            continue;
+        }
+        
+        if(clientsFD.size() == 10 ) {
+            mtxScreen.lock();
+            cout<<"conexiones llenas"<<endl;
+            mtxScreen.unlock();
+            continue;
+        }
+
+        mtxScreen.lock();
+        clientsFD.push_back(newfd);
+        mtxScreen.unlock();
+        
+        thread (clientHandler,newfd).detach();        
+    }
+ 
+    return 0;
+} 
